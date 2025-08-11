8e2778af794b0bdcaef4cf40ee1005917099fdda
Hemanth <hemanthachari860@gmail.com>
2025-08-11 23:32:24 +0530

    Initial commit: Set up project skeleton with functions and main loop

---
+++ contacts.py
@@ -0,0 +1,41 @@
+contacts=[]
+
+def add_contact():
+    pass
+
+def view_contact():
+    pass
+
+def search_contact():
+    pass
+
+print("--- WELCOME TO YOUR CONTACT BOOK ---")
+
+while(True):
+    print("\nPlease select an option")
+    print("1. Add a new contact")
+    print("2. View all contacts")
+    print("3. Search for a contact")
+    print("4. Quit")
+
+    try:
+        choice=int(input("Enter your choice (1-4):"))
+
+        if choice==1:
+            add_contact()
+
+        elif choice==2:
+            view_contact()
+
+        elif choice==3:
+            search_contact()
+
+        elif choice==4:
+            print("GOODBYE!!")
+            break
+        else:
+            print("Invalid choice.Please try again.Enter a number from 1 to 4 only")
+
+    except ValueError:
+        print("Invalid input!! Please enter a number,not text.")
+
